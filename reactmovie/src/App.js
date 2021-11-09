7ce4b5cb2bacb97eb8582dde5ca699e52145a50c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-09 16:20:09 -0500

    Using async await to simplify the code

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -7,20 +7,18 @@ function App() {
 
   const [movies, setMovies] = useState([])
 
-  const fetchMovies = () => {
-    fetch('https://swapi.dev/api/films/').then(res => {
-      return res.json()
-    }).then(data => {
-      const transformMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformMovies)
+  async function fetchMovies() {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json()
+    const transformMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
     })
+    setMovies(transformMovies)
   }
 
   return (
