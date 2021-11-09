ab3f2553c3d0b35750ff48ed258f64e36b2c0327
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-09 11:03:58 -0500

    fetching data from the swapi api and then creating useState hook and setting the data.results to the updating state and then adding the movies state to the movies prop from the MoviesList component

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/movielist/MoviesList'
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = () => {
+    fetch('https://swapi.dev/api/films/').then(res => {
+      return res.json()
+    }).then(data => {
+      setMovies(data.results)
+    })
+  }
 
   return (
     <>
@@ -25,7 +21,7 @@ function App() {
         <button>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </>
   );
