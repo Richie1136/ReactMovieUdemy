27c4272a25751080e283c1a948e12dcfe1b14df3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-09 16:36:58 -0500

    Added a try catch block to my fetchMovies function in case of any errors that may occur while fetching data

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -8,20 +8,24 @@ function App() {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
 
-  async function fetchMovies() {
-    setLoading(true)
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
-    const transformMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      }
-    })
-    setMovies(transformMovies)
-    setLoading(false)
+  const fetchMovies = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json()
+      const transformMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformMovies)
+      setLoading(false)
+    } catch (err) {
+      return err
+    }
   }
 
   return (
